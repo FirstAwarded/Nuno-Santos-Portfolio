eb840a3dcf460fac8afe4aa0a801c13eb3d8fd8d
FirstAwarded <nunorgsantos04@gmail.com>
2025-09-13 00:04:55 +0000

    fixed import libraries connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Work from "./pages/Work";
@@ -10,22 +13,35 @@ import Components from "./pages/Components";
 import NotFound from "./pages/NotFound";
 import Oart from "./pages/Oart";
 
+// Create a QueryClient instance (shared across the app)
+const queryClient = new QueryClient();
+
 const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/work/uber-train-integration" element={<CaseStudy />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/components" element={<Components />} />
-        <Route path="/work/oart" element={<Oart />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      {/* Global toast systems */}
+      <Toaster />
+      <Sonner />
+
+      {/* Router */}
+      <BrowserRouter>
+        <Routes>
+          {/* Main pages */}
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/components" element={<Components />} />
+          <Route path="/work" element={<Work />} />
+
+          {/* Case studies */}
+          <Route path="/work/uber-train-integration" element={<CaseStudy />} />
+          <Route path="/work/oart" element={<Oart />} />
+
+          {/* Catch-all (404) */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
